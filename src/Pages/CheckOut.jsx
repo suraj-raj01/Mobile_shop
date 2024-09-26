00abd260e2278182f56b1c6af591e01c57293c16
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-26 23:52:00 +0530

    CheckOut Page

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -1,9 +1,102 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+
+import { useDispatch } from "react-redux";
+import {addCartData} from "../addToCartSlice";
+import { useParams } from "react-router-dom";
 const CheckOut = () =>{
+    const dispatch = useDispatch();
+    const [mydata, setMydata] = useState([]);
+    const {id} = useParams();
+  
+    const loadData = () => {
+      let api = `http://localhost:3000/products/?id=${id}`;
+      axios.get(api).then((res) => {
+        setMydata(res.data);
+      });
+    };
+    useEffect(() => {
+      loadData();
+    }, []);
+  
+    const addDataToCart = (id,name,model,brand,OS,price,img,desc) =>{
+      dispatch(
+        addCartData({
+          id:id,
+          name:name,
+          model:model,
+          brand:brand,
+          OS:OS,
+          price:price,
+          img:img,
+          description:desc,
+          qnty:1
+        })
+      );
+    };
+  
+    const res = mydata.map((key) => {
+    const newPrice = key.price-(key.price*key.discount/100).toFixed(0);
+      return (
+        <>
+          <Container>
+            <div
+              id="products"
+              style={{
+                display: "flex",
+                padding: "20px",
+                width: "100%",
+                alignItems: "center",
+              }}
+            >
+              <div id="box1">
+                <img src={key.img} alt="" width="300px" height="300px"/>
+              </div>
+              <div id="box2">
+                <h3>{key.name}</h3>
+                <b>{key.description}</b>
+                <br />
+                <br />
+                <span>Model : {key.model}</span>
+                <br />
+                <span>Brand : {key.brand}</span>
+                <br />
+                <span>OS : {key.OS}</span>
+                <br />
+                <span>
+                  Price : {newPrice}
+                  {".00 ₹"}
+                </span>
+                <br />
+                <br />
+                <Button variant="outline-primary"
+                onClick={()=>{
+                  addDataToCart(
+                    key.id,
+                    key.name,
+                    key.model,
+                    key.brand,
+                    key.OS,
+                    newPrice,
+                    key.img,
+                    key.description
+                  )
+                }}
+                >Add to Cart</Button>
+              </div>
+            </div>
+            <br />
+          </Container>
+        </>
+      );
+    });
     return(
         <>
-            <div id="header">
-                <h1>Checkout Page</h1>
-            </div>
+        <div id="header">
+        <h1>CheckOut</h1>
+        </div>
+        <div id="product">{res}</div>
         </>
     )
 }
