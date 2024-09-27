e5941bc775b391c2f7b2b0c8ff034ba40074624d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-28 00:52:43 +0530

    Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -902,6 +902,119 @@ const Home = () => {
           <button id="search-btn" onClick={catValHandle}>
             Search
           </button>
+          <br /><br />
+          <p>Customer Reviews </p>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>See all Reviews </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>⭐⭐⭐⭐⭐ <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" />  and up</span>{" "} 
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>⭐⭐⭐⭐ <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" />  and up</span>{" "} 
+          </div>
+          <div id="search">
+            <Form.Check
+            
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>⭐⭐⭐ <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" />  and up</span>{" "} 
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>⭐⭐ <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" />  and up</span>{" "} 
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>⭐ <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" /> <img src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain" height='20px' width='20px' alt="" />  and up</span>{" "} 
+          </div>
+              <br />
+          <p>Offers </p>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>See all Offers</span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>10% Off or more</span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>15% Off or more</span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>20% Off or more</span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>25% Off or more</span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check
+              
+              type="radio"
+              name="nm"
+              aria-label="radio 1"
+            />
+            <span>30% Off or more</span>{" "}
+          </div>
+        <br /><br />
         </div>
         {isVisible ? (
           <div id="home-product">{res}</div>
