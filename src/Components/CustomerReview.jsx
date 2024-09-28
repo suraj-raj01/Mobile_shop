9efecffa3571ec40211e76a1deb839e9f23b5b15
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-28 17:26:01 +0530

    Customer review Page

---
+++ src/Components/CustomerReview.jsx
@@ -0,0 +1,138 @@
+import Form from "react-bootstrap/Form";
+const CustomerReview = () =>{
+    return(
+        <>
+        <p>Customer Reviews </p>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>See all Reviews </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>
+              ⭐⭐⭐⭐⭐{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              and up
+            </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>
+              ⭐⭐⭐⭐{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              and up
+            </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>
+              ⭐⭐⭐{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              and up
+            </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>
+              ⭐⭐{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              and up
+            </span>{" "}
+          </div>
+          <div id="search">
+            <Form.Check type="radio" name="nm" aria-label="radio 1" />
+            <span>
+              ⭐{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              <img
+                src="https://th.bing.com/th/id/OIP.6GHvGFJvbmo5Nf8b_I9g8AAAAA?rs=1&pid=ImgDetMain"
+                height="20px"
+                width="20px"
+                alt=""
+              />{" "}
+              and up
+            </span>{" "}
+          </div>
+        </>
+    )
+}
+export default CustomerReview;
