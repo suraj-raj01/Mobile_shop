8085624115fb736a3261623bb46a416bc3fc714d
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-28 17:25:42 +0530

    Color Page

---
+++ src/Components/Colors.jsx
@@ -0,0 +1,18 @@
+const Color = () =>{
+    return(
+        <>
+        <p>Colors</p>
+          <div id="search">
+            <div id="clr-box" style={{backgroundColor:'red'}}></div>
+            <div id="clr-box" style={{backgroundColor:'grey'}}></div>
+            <div id="clr-box" style={{backgroundColor:'pink'}}></div>
+            <div id="clr-box" style={{backgroundColor:'blue'}}></div>
+            <div id="clr-box" style={{backgroundColor:'orange'}}></div>
+            <div id="clr-box" style={{backgroundColor:'skyblue'}}></div>
+            <div id="clr-box" style={{backgroundColor:'black'}}></div>
+            <div id="clr-box" style={{backgroundColor:'white'}}></div>
+          </div>
+        </>
+    )
+}
+export default Color;
