5c8cc6b1ad9fc3d7c40303c4c7b99fcd0d8232d0
SURAJ_KUMAR <surajk38656@gmail.com>
2024-10-29 23:46:43 +0530

    Brand Laptop

--- src/Brand_Laptop/Hp.jsx
+++ src/Brand_Laptop/Hp.jsx
@@ -52,6 +52,7 @@ const HP = () => {
     navigate(`/checkout/${id}`)
 }
   const res = mydata.map((key) => {
+    const rating = Math.floor(Math.random()*1000)+50;
     return (
       <>
         <Container>
@@ -74,7 +75,7 @@ const HP = () => {
               <div style={{display:'flex',alignItems:'center',justifyContent:'start',gap:'2px',padding:'10px 0px',color:'goldenrod'}}>
               <span style={{padding:'1px 10px 0px 0px',fontWeight:'bold',fontSize:'14px',color:'black'}}>3.5</span> 
               <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star-half-stroke"></i><i class="fa-regular fa-star"></i>
-              <span style={{padding:'1px 0px 0px 10px',fontSize:'14px',color:'black',textTransform:'lowercase',color:'#183961'}}>1,167 ratings</span>
+              <span style={{padding:'1px 0px 0px 10px',fontSize:'14px',color:'black',textTransform:'lowercase',color:'#183961'}}>{rating} ratings</span>
               </div>
               <span>Model : {key.model}</span>
               <br />
