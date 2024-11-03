2df9f835c893a78e5f562e968a850108732717ac
SURAJ_KUMAR <surajk38656@gmail.com>
2024-11-03 20:42:13 +0530

    Dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -7,12 +7,14 @@ const Dashboard = () => {
   const navigate = useNavigate();
   const [username, setUserName] = useState("");
   const [usermail, setUserEmail] = useState("");
+  const [usermobile, setUserMobile] = useState("");
   useEffect(() => {
     const name = localStorage.getItem("username");
     const email = localStorage.getItem("usermail");
+    const mobile = localStorage.getItem("usermobile");
     setUserName(name);
     setUserEmail(email);
-    console.log(usermail);
+    setUserMobile(mobile);
   }, []);
   const logOut = () => {
     localStorage.clear();
@@ -21,28 +23,84 @@ const Dashboard = () => {
   };
 
   // on mouse enter camera icon will appear
-  const camera = () =>{
-    document.getElementById("camera").style.display="block";
-  }
+  const camera = () => {
+    document.getElementById("camera").style.display = "block";
+  };
   // on mouse leave camera icon will disappear
-  const camera1 = () =>{
-    document.getElementById("camera").style.display="none";
-  }
+  const camera1 = () => {
+    document.getElementById("camera").style.display = "none";
+  };
 
+  // complete your profile
+  const completeProf = () => {
+    navigate("/register");
+  };
 
   return (
     <>
       <div id="main">
         <div id="box1">
-            <div id="profile" onMouseOver={camera} onMouseLeave={camera1} >
-              {/* camera iconn */}
+          <div id="profile" onMouseOver={camera} onMouseLeave={camera1}>
+            {/* camera iconn */}
+
             <div id="camera">
-            <i class="fa-solid fa-camera"></i>
+              <i class="fa-solid fa-camera"></i>
             </div>
-            </div>
-            <br />
-            <h4 style={{fontWeight:'bold'}}>{username}</h4>
-            <p style={{textTransform:'lowercase'}}>{usermail}</p>
+          </div>
+          <br />
+          <h3 style={{ fontWeight: "bold",textTransform:'capitalize' }}>{username}</h3>
+          <hr />
+          <div
+            style={{
+              width: "90%",
+              margin: "auto",
+              // boxShadow:'0px 0px .9px',
+              fontSize: "14px",
+              textAlign: "start",
+              height: "185px",
+              padding: "0px 20px",
+              display: "flex",
+              flexDirection: "column",
+              borderRadius: "7px",
+              gap: "5px",
+            }}
+          >
+            <span style={{ textTransform: "lowercase" }}>
+              Email : {usermail}
+            </span>
+            <span style={{ textTransform: "lowercase" }}>
+              Mobile : {usermobile}
+            </span>
+          </div>
+          <div
+            style={{
+              height: "40px",
+              width: "100%",
+              // border:'1px solid',
+              textAlign: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems:'center',
+              justifyContent:'center',
+              // margin:'2px',
+              backgroundColor:'whitesmoke',
+              borderBottomRightRadius:'5px',
+              borderBottomLeftRadius:'5px'
+            }}
+          >
+            <span
+              style={{
+                textTransform: "capitalize",
+                color: "deepskyblue",
+                cursor: "pointer",
+                fontWeight: "700",
+                fontSize:'14px'
+              }}
+              onClick={completeProf}
+            >
+              complete your profile{" "}
+            </span>
+          </div>
         </div>
         <div id="box2">
           <h2 style={{ color: "#183961", fontWeight: "600" }}>
