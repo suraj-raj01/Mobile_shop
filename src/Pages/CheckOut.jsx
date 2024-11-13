a026fce868d52d8e7b76c06bf73e0fe231f108ae
SURAJ_KUMAR <surajk38656@gmail.com>
2024-11-13 11:40:15 +0530

    Checout Page

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -5,14 +5,15 @@ import Form from "react-bootstrap/Form";
 
 import { useDispatch } from "react-redux";
 import { addCartData } from "../addToCartSlice";
-import { useParams, useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const CheckOut = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [mydata, setMydata] = useState([]);
+  const [payopt,setPayOpt] = useState("");
   const { id } = useParams();
-
+  console.log(payopt)
   const loadData = () => {
     let api = `http://localhost:3000/products/?id=${id}`;
     axios.get(api).then((res) => {
@@ -23,12 +24,12 @@ const CheckOut = () => {
     loadData();
   }, []);
 
-  const detail = (id) =>{
-    navigate(`/details/${id}`)
-  }
-  const handleSubmit=()=>{
-    navigate("/payments")
-  }
+  const detail = (id) => {
+    navigate(`/details/${id}`);
+  };
+  const handleSubmit = () => {
+    navigate(`/payments/${payopt}`);
+  };
   const addDataToCart = (id, name, model, brand, OS, price, img, desc) => {
     dispatch(
       addCartData({
@@ -57,69 +58,79 @@ const CheckOut = () => {
     let im = document.getElementById("orgImg");
     im.src = img;
   };
-  const showBigImg = (img) =>{
+  const showBigImg = (img) => {
     let bigImg = document.getElementById("bigimg");
     bigImg.src = img;
-    document.getElementById("bigimg").style.display="block"
-  }
-  const hideBigImg = () =>{
-    document.getElementById("bigimg").style.display="none"
-  }
+    document.getElementById("bigimg").style.display = "block";
+  };
+  const hideBigImg = () => {
+    document.getElementById("bigimg").style.display = "none";
+  };
 
   const res = mydata.map((key) => {
     const newPrice = key.price - ((key.price * key.discount) / 100).toFixed(0);
     return (
       <>
         <Container>
-          <div
-            id="checkout-products"
-          >
+          <div id="checkout-products">
             <div id="checkout-img">
-                <div id="imgbox">
-                  <img
-                    src={key.img1}
-                    onMouseEnter={() => {
-                      showImg1(key.img1);
-                    }}
-                    alt="error!"
-                    height="50px"
-                    width="55px"
-                    style={{ padding: "0px 5px",color:'red'}}
-                  />
-                </div>
-
-                <div id="imgbox">
-                  <img
-                    src={key.img2}
-                    onMouseEnter={() => {
-                      showImg2(key.img2);
-                    }}
-                    alt="error!"
-                    height="50px"
-                    width="55px"
-                    style={{ padding: "0px 5px",color:'red'}}
-                  />
-                </div>
-
-                <div id="imgbox">
-                  <img
-                    src={key.img}
-                    onMouseEnter={() => {
-                      showImg3(key.img);
-                    }}
-                    alt=""
-                    height="50px"
-                    width="55px"
-                    style={{ padding: "0px 5px" }}
-                  />
-                </div>
+              <div id="imgbox">
+                <img
+                  src={key.img1}
+                  onMouseEnter={() => {
+                    showImg1(key.img1);
+                  }}
+                  alt="error!"
+                  height="50px"
+                  width="55px"
+                  style={{ padding: "0px 5px", color: "red" }}
+                />
               </div>
 
+              <div id="imgbox">
+                <img
+                  src={key.img2}
+                  onMouseEnter={() => {
+                    showImg2(key.img2);
+                  }}
+                  alt="error!"
+                  height="50px"
+                  width="55px"
+                  style={{ padding: "0px 5px", color: "red" }}
+                />
+              </div>
 
-            <div id="box1" onMouseOver={()=>{showBigImg(key.img)}} onMouseLeave={hideBigImg}>
-              <img src={key.img} id="orgImg" alt="" width="320px" height="320px" onClick={() => {
-                      detail(key.id);
-                    }}/>
+              <div id="imgbox">
+                <img
+                  src={key.img}
+                  onMouseEnter={() => {
+                    showImg3(key.img);
+                  }}
+                  alt=""
+                  height="50px"
+                  width="55px"
+                  style={{ padding: "0px 5px" }}
+                />
+              </div>
+            </div>
+
+            <div
+              id="box1"
+              onMouseOver={() => {
+                showBigImg(key.img);
+              }}
+              onMouseLeave={hideBigImg}
+            >
+              <img
+                src={key.img}
+                id="orgImg"
+                alt=""
+                width="320px"
+                height="320px"
+                onClick={() => {
+                  detail(key.id);
+                }}
+              />
             </div>
             <div id="box2">
               <h3>{key.name}</h3>
@@ -157,44 +168,84 @@ const CheckOut = () => {
               </Button>
             </div>
             <div id="checkout-payment">
-              <h5>Select a Payment Method</h5>
-              <br />
-              <div id="opt">
-                <span>Debit or Credit Card</span>{" "}
-                <Form.Check type="radio" name="nm" aria-label="radio 1" />
-              </div>
-              <div id="opt">
-                <span>Net Banking</span>{" "}
-                <Form.Check type="radio" name="nm" aria-label="radio 1" />
-              </div>
-              <Form.Select aria-label="Default select example" style={{marginTop:'5px'}}>
-                <option>Choose an Option</option>
-                <hr />
-                <option value="1">AXIS Bank</option>
-                <option value="2">BOB</option>
-                <option value="3">BOI</option>
-                <option value="4">Central Bank Of India</option>
-                <option value="5">HDFC</option>
-                <option value="6">India Bank</option>
-                <option value="7">IDFC</option>
-                <option value="8">IndusInd Bank</option>
-                <option value="9">Kotak Mahindra Bank</option>
-                <option value="10">Punjab National Bank</option>
-                <option value="11">SBI</option>
-              </Form.Select>
-              <div id="opt">
-                <span>Other UPI Apps</span>{" "}
-                <Form.Check type="radio" name="nm" aria-label="radio 1" />
-              </div>
-              <div id="opt">
-                <span>EMI</span>{" "}
-                <Form.Check type="radio" name="nm" aria-label="radio 1" />
-              </div>
-              <div id="opt">
-                <span style={{color:'gray'}}>Cash on Delivery <br /> uavailabe!! now</span>{" "}
-                <Form.Check type="radio" name="nm" aria-label="radio 1" disabled/>
-              </div>
-              <button id="btn" onClick={handleSubmit}>Use this payment method</button>
+              <Form>
+                <h5>Select a Payment Method</h5>
+                <br />
+                <div id="opt">
+                  <span>Debit or Credit Card</span>{" "}
+                  <Form.Check
+                    type="radio"
+                    name="nm"
+                    value="debit & credit"
+                    onChange={(e) => {
+                      setPayOpt(e.target.value);
+                    }}
+                    aria-label="radio 1"
+                  />
+                </div>
+                <div id="opt">
+                  <span>Net Banking</span>{" "}
+                  <Form.Check type="radio"
+                    name="nm"
+                    value="netbanking"
+                    onChange={(e) => {
+                    setPayOpt(e.target.value);
+                    }}
+                    aria-label="radio 1" />
+                </div>
+                {/* <Form.Select
+                  aria-label="Default select example"
+                  style={{ marginTop: "5px" }}
+                >
+                  <option>Choose an Option</option>
+                  <hr />
+                  <option value="1">AXIS Bank</option>
+                  <option value="2">BOB</option>
+                  <option value="3">BOI</option>
+                  <option value="4">Central Bank Of India</option>
+                  <option value="5">HDFC</option>
+                  <option value="6">India Bank</option>
+                  <option value="7">IDFC</option>
+                  <option value="8">IndusInd Bank</option>
+                  <option value="9">Kotak Mahindra Bank</option>
+                  <option value="10">Punjab National Bank</option>
+                  <option value="11">SBI</option>
+                </Form.Select> */}
+                <div id="opt">
+                  <span>Other UPI Apps</span>{" "}
+                  <Form.Check   type="radio"
+                   name="nm"
+                   value="upi"
+                   onChange={(e) => {
+                   setPayOpt(e.target.value);
+                   }}
+                  aria-label="radio 1" />
+                </div>
+                <div id="opt">
+                  <span>EMI</span>{" "}
+                  <Form.Check   type="radio"
+                   name="nm"
+                   value="emi"
+                   onChange={(e) => {
+                   setPayOpt(e.target.value);
+                   }}
+                  aria-label="radio 1" />
+                </div>
+                <div id="opt">
+                  <span style={{ color: "gray" }}>
+                    Cash on Delivery <br /> uavailabe!! now
+                  </span>{" "}
+                  <Form.Check
+                    type="radio"
+                    name="nm"
+                    aria-label="radio 1"
+                    disabled
+                  />
+                </div>
+                <button id="btn" onClick={handleSubmit}>
+                  Use this payment method
+                </button>
+              </Form>
             </div>
           </div>
           <br />
