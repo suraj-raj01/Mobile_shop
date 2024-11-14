bf172a68dfc6128e5173071a9d423f66e0740b44
SURAJ_KUMAR <surajk38656@gmail.com>
2024-11-14 13:14:35 +0530

    Payment page

--- src/Pages/Payments.jsx
+++ src/Pages/Payments.jsx
@@ -1,59 +1,82 @@
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const Payment = () => {
-  const {payopt} = useParams();
-  console.log(payopt)
-  if(payopt.includes("debit")){
-    return(
+  const { payopt } = useParams();
+  console.log(payopt);
+  if (payopt.includes("debit")) {
+    return (
       <>
-      <br />
-      <div id="payment_method">
-        <p>Paymennt Option {payopt} Card</p>
-        <div id="cards">
-          <input type="text" placeholder="Name on Card" />
-          <input type="text" placeholder="Debit / Credit Card Number" />
-          <input type="text" placeholder="Expiration" />
-          <input type="text" placeholder="CVV" />
-          <Button variant="outline-primary">Conform</Button>
+        <br />
+        <div id="payment_method">
+          <p>Paymennt Option {payopt} Card</p>
+          <Form>
+            <div id="cards">
+              <input type="text" placeholder="Name on Card" />
+              <input type="text" placeholder="Debit / Credit Card Number" />
+              <input type="text" placeholder="Expiration" />
+              <input type="text" placeholder="CVV" />
+              <Button variant="outline-primary">Conform</Button>
+            </div>
+          </Form>
         </div>
-      </div>
-      <br />
+        <br />
       </>
-    )
+    );
   }
-  if(payopt.includes("netbanking")){
-    return(
+  if (payopt.includes("netbanking")) {
+    return (
       <>
-      <br />
-      <div id="payment_method">
-      <p>Paymennt Option {payopt}</p>
-      </div>
-      <br />
+        <br />
+        <div id="payment_method">
+          <p>Paymennt Option {payopt}</p>
+          <Form>
+            <div id="cards">
+              <Form.Select>
+                <option>Select a Bank</option>
+                <option value="1">Kotak Mahindra</option>
+                <option value="2">SBI</option>
+                <option value="3">BOB</option>
+                <option value="3">PNB</option>
+                <option value="3">HDFC Bank</option>
+                <option value="3">IDFC Bank</option>
+                <option value="3">ICICI Bank</option>
+                <option value="3">Axis Bank</option>
+                <option value="3">Kenara Bank</option>
+                <option value="3">India Bank</option>
+              </Form.Select>
+              <input type="text" placeholder="Bank Holder Name" />
+              <input type="text" placeholder="IFSC Code" />
+              <input type="text" placeholder="Account Number" />
+              <Button variant="outline-primary">Conform</Button>
+            </div>
+          </Form>
+        </div>
+        <br />
       </>
-    )
+    );
   }
-  if(payopt.includes("upi")){
-    return(
+  if (payopt.includes("upi")) {
+    return (
       <>
-      <br />
-      <div id="payment_method">
-      <p>Paymennt Option {payopt}</p>
-      </div>
-      <br />
+        <br />
+        <div id="payment_method">
+          <p>Paymennt Option {payopt}</p>
+        </div>
+        <br />
       </>
-    )
+    );
   }
-  if(payopt.includes("emi")){
-    return(
+  if (payopt.includes("emi")) {
+    return (
       <>
-      <br />
-      <div id="payment_method">
-      <p>Paymennt Option {payopt}</p>
-      </div>
-      <br />
+        <br />
+        <div id="payment_method">
+          <p>Paymennt Option {payopt}</p>
+        </div>
+        <br />
       </>
-    )
+    );
   }
   return (
     <>
