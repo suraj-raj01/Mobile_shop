ae9e7916a487185ccbc9cef6e961641a34cb8b50
SURAJ_KUMAR <surajk38656@gmail.com>
2024-11-18 14:28:45 +0530

    products

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -13,16 +13,19 @@ const Products = () =>{
         alignItems:'center',
         justifyContent:'center',
         width:'100%',
-        padding:'40px 0px 30px 0px',
+        padding:'30px 0px 20px 0px',
         fontWeight:'600',
         gap:'10px',
-        fontSize:'20px',
-        color:'#183961'
+        fontSize:'18px',
+        marginTop:'10px',
+        backgroundColor:'#183961'
         }}>
-      <Nav.Link as={Link} to="allproducts">All Products</Nav.Link> |
+      <div id="product_lyt">
+      <Nav.Link as={Link} to="allproducts">All Products</Nav.Link>
       <Nav.Link as={Link} to="premiumproducts"><i class="fa-brands fa-square-web-awesome"></i> Premium  Products</Nav.Link>
       </div>
-      <hr />
+      </div>
+      <br />
     <div>
       <Outlet/>
     </div>
