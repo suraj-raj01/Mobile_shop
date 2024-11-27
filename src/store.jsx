5101cd6eb4a5810cfce34b40c6a3ef8ab0ba183b
SURAJ_KUMAR <surajk38656@gmail.com>
2024-11-28 01:42:45 +0530

    Store

--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mydata from "./addToCartSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import myReducer from "./addToCartSlice";
+
+const persistConfig = {
+    key: "root",
+    storage,
+  };
+const persistedReducer = persistReducer(persistConfig, myReducer);
 const store = configureStore({
     reducer:{
-        addCart:mydata
+        addCart:persistedReducer
     }
 })
 export default store;
+export const persitor = persistStore(store);
